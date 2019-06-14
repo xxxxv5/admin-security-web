2e3a3ca86193ebdf8cdae0e01db32b48b0efb985
charlie <charliephy2018@gmail.com>
2019-06-14 10:45:56 +0800

    first commits-第一次提交

---
+++ src/views/permission/directive.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="app-container">
+    <el-card class="box-card">
+      <el-table
+        border
+        stripe
+        :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      >
+        <el-table-column label="日期" prop="date" align="center" />
+        <el-table-column label="名字" prop="name" align="center" />
+        <el-table-column label="操作" align="center">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)"
+            >修改</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DirectivePermission',
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      search: ''
+    }
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/permission/page.vue
@@ -0,0 +1,88 @@
+<template>
+  <!-- form表单-->
+  <div class="app-container">
+    <el-card class="box-card">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="审批人">
+          <el-input v-model="formInline.user" size="mini" placeholder="审批人" />
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="formInline.region" size="mini" placeholder="活动区域">
+            <el-option label="区域一" value="shanghai" />
+            <el-option label="区域二" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="mini" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- form表单-->
+      <!-- table表格-->
+      <el-table
+        border
+        stripe
+        :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      >
+        <el-table-column label="日期" prop="date" align="center" />
+        <el-table-column label="名字" prop="name" align="center" />
+        <el-table-column label="操作" align="center">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)"
+            >修改</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- table表格-->
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PagePermission',
+  data() {
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      search: ''
+    }
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    },
+    onSubmit() {
+      console.log('submit!')
+    }
+  }
+}
+</script>
